409405a9cb11789c3926da5c40506a16359b0835
verus-fastrun <47342110+verus-fastrun@users.noreply.github.com>
2022-02-10 10:04:31 -0500

    Updated App.js with link to Speed.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Length from "./Components/Conversion/Length";
 import Mass from "./Components/Conversion/Mass";
 import Temperature from "./Components/Conversion/Temperature";
+import Speed from "./Components/Conversion/Speed";
 import React from "react";
 import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';
 
@@ -17,18 +18,29 @@ function App() {
       //Another place for dropdowns is: https://reactjs.org/docs/forms.html
       // routed to reach different components
     <>
+    <div>Conversions
     <Router>
+    <li>
     <Link to ="/">Length</Link>
-    <br></br>
-    <Link to = "/Mass">Mass</Link>
-     <br></br>
+    </li>
+    <li>
+    <Link to = "/Mass">Mass/Weight</Link>
+    </li>
+    <li>
     <Link to = "/Temperature">Temperature</Link>
+    </li>
+    <li>
+    <Link to = "/Speed">Speed</Link>
+    </li>
     <Routes>
       <Route exact path = '/' element={<Length />}></Route>
       <Route exact path = '/Mass' element={<Mass />}></Route>
       <Route exact path = '/Temperature' element={<Temperature />}></Route>
+      <Route exact path = '/Speed' element={<Speed />}></Route>
     </Routes>
+
       </Router>
+      </div>
       </>
   );
 }
