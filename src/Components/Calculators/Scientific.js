ee17da138325c58d9f39175e43143d2829005ed6
joshharrin <53531213+joshharrin@users.noreply.github.com>
2022-02-27 12:03:08 -0500

    Update Scientific.js
    removes pending calculation result display according to feedback. minimal design

--- src/Components/Calculators/Scientific.js
+++ src/Components/Calculators/Scientific.js
@@ -70,7 +70,6 @@ const Scientific = () => {
             <div className="App1">
                 <div className="sci_calculator">
                     <div className="display">
-                        {result ? <span>({result})</span> : ''}&nbsp;
                         {calc || "0"}
                     </div>
                 
