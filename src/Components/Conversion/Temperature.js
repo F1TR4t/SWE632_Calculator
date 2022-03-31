562dad1889ccdefd91d330fdefae48e63088b8be
verus-fastrun <47342110+verus-fastrun@users.noreply.github.com>
2022-03-31 19:56:31 -0400

    Added background color
    Temporarily set to white

--- src/Components/Conversion/Temperature.js
+++ src/Components/Conversion/Temperature.js
@@ -80,6 +80,7 @@ import React, { useState , useEffect} from 'react';
             border: '1px solid rgba(0, 0, 0, 1.00)', 
             width: '300px',
             height: '240px',
+            background:'white',
        };
 
     const conversionGuide = {
